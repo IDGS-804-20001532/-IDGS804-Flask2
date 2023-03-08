a1332d1e21c2b3a6dc6fe2ea817205905f5cf533
Adrian Hernandez <hernandezsanchezluisadrian1@gmail.com>
2023-03-08 16:29:26 -0600

    Agregacion de tarea Resistencias. 08_03_2023

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from flask_wtf.csrf import CSRFProtect
 from collections import Counter
 from flask import make_response
 from flask import flash
+import math
+
 
 
 
@@ -29,7 +31,7 @@ def alumnos():
         print(reg_alumn.nombre.data)
     return render_template("Alumnos2.html",  form = reg_alumn, datos=datos)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/Cajas', methods=['GET', 'POST'])
 def caja():
     if request.method == 'GET':
         reg_form = forms.BoxForm()
@@ -134,6 +136,104 @@ def traductorR():
 
     return render_template("TraductorR.html",form=reg_traductor)
 
+def calcular_resistencia(banda1, banda2, banda3, tolerancia):
+
+    valores = {
+        "negro": 0,
+        "marron": 1,
+        "rojo": 2,
+        "naranja": 3,
+        "amarillo": 4,
+        "verde": 5,
+        "azul": 6,
+        "violeta": 7,
+        "gris": 8,
+        "blanco": 9
+    }
+    english_names = {
+        "negro": "black",
+        "marron": "brown",
+        "rojo": "red",
+        "naranja": "orange",
+        "amarillo": "yellow",
+        "verde": "green",
+        "azul": "blue",
+        "violeta": "violet",
+        "gris": "gray",
+        "blanco": "white",
+        "oro": "gold",
+        "plata": "silver"
+    }
+    banda1_en = english_names[banda1]
+    banda2_en = english_names[banda2]
+    banda3_en = english_names[banda3]
+    tolerancia_en = english_names[tolerancia]
+
+    valor1 = valores[banda1]
+    valor2 = valores[banda2]
+    multiplicador = math.pow(10, valores[banda3])
+    tolerancia_valor = 0.05 if tolerancia == "oro" else 0.1
+
+    valor = (valor1 * 10 + valor2) * multiplicador
+    valor_minimo = valor * (1 - tolerancia_valor)
+    valor_maximo = valor * (1 + tolerancia_valor)
+
+    return {
+        "colorBanda1": banda1_en,
+        "colorBanda2": banda2_en,
+        "colorBanda3": banda3_en,
+        "colorTolerancia": tolerancia_en,
+        "banda1": banda1,
+        "banda2": banda2,
+        "banda3": banda3,
+        "tolerancia": tolerancia,
+        "valor": valor,
+        "valor_minimo": valor_minimo,
+        "valor_maximo": valor_maximo
+    }
+
+
+@app.route('/', methods=['GET'])
+def index():
+    form = forms.ResistenciaForm(request.form)
+
+    with open("Resistencia.txt", "r") as f:
+        valores_guardados = [line.strip().split(",") for line in f]
+
+    resultados_guardados = []
+    for valores in valores_guardados:
+        if len(valores) == 4:
+            resultado_guardado = calcular_resistencia(*valores)
+            resultados_guardados.append(resultado_guardado)
+
+    return render_template('resistencia.html', form=form, resultados_guardados=resultados_guardados)
+
+
+@app.route('/', methods=['POST'])
+def calcular():
+    form =forms.ResistenciaForm(request.form)
+    banda1 = request.form['banda1']
+    banda2 = request.form['banda2']
+    banda3 = request.form['banda3']
+    tolerancia = request.form['tolerancia']
+
+    resultado = calcular_resistencia(banda1, banda2, banda3, tolerancia)
+
+    valores_guardados = []
+    with open("Resistencia.txt", "r") as f:
+        for line in f:
+            valores = line.strip().split(",")
+            if len(valores) == 4:
+                resultado_guardado = calcular_resistencia(*valores)
+                valores_guardados.append(resultado_guardado)
+                print(valores_guardados)
+
+    valores_guardados.append(resultado)
+
+    with open("Resistencia.txt", "a") as f:
+        f.write(",".join([banda1, banda2, banda3, tolerancia]) + "\n")
+
+    return render_template('resistencia.html', resultado=resultado, form=form, valores_guardados=valores_guardados)
 
 
 if __name__=="__main__":
--- forms.py
+++ forms.py
@@ -1,8 +1,9 @@
 from wtforms import Form
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, FieldList,FormField, RadioField,IntegerField 
+from wtforms import StringField, SubmitField, FieldList,FormField, RadioField,IntegerField,SelectField
 from wtforms.fields import EmailField
 from wtforms import validators
+
 
 def mi_validacion(form, field):
     if len(field.data) ==0:
@@ -41,3 +42,15 @@ class LoginForm(Form):
     validators.length(min=5,max=10,message='Ingesa un min 5 max 10')])
     password=StringField('Contraseña',[validators.DataRequired(message='El campo Contraseña es requerido'),
     validators.length(min=5,max=10,message='Ingesa un min 5 max 10')])
+class ResistenciaForm(FlaskForm):
+    opciones_banda = [("negro", "Negro"), ("marron", "Marrón"), ("rojo", "Rojo"), ("naranja", "Naranja"),
+                      ("amarillo", "Amarillo"), ("verde", "Verde"), ("azul", "Azul"), ("violeta", "Violeta"),
+                      ("gris", "Gris"), ("blanco", "Blanco")]
+
+    opciones_tolerancia = [("oro", "Oro"), ("plata", "Plata")]
+
+    banda1 = SelectField("Banda 1", choices=opciones_banda, validators=[validators.DataRequired()])
+    banda2 = SelectField("Banda 2", choices=opciones_banda, validators=[validators.DataRequired()])
+    banda3 = SelectField("Banda 3", choices=opciones_banda, validators=[validators.DataRequired()])
+    tolerancia = RadioField("Tolerancia", choices=opciones_tolerancia, validators=[validators.DataRequired()])
+    calcular = SubmitField("Calcular")
